c85033d6a786ada745c260835ed17234544aad87
IasArchi <iasmimcardoso11@gmail.com>
2022-05-05 19:17:02 -0300

    gftstartwoman

--- src/br/com/gftstartwoman/projetobancodigital/Cliente.java
+++ src/br/com/gftstartwoman/projetobancodigital/Cliente.java
@@ -1,7 +1,19 @@
 package br.com.gftstartwoman.projetobancodigital;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
     private String nome;
+    private List<Conta>contas = new ArrayList<>();
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
+    }
 
     public String getNome() {
         return nome;
